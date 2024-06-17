01cc253f6cc230acc510ccc1e10c6311bd430eed
Rodrigo Avancini <rodrigo.avancini@thoughtful.ai>
2024-06-17 10:53:26 -0300

    better enum

--- Cor.java
+++ Cor.java
@@ -1,3 +1,18 @@
+import java.awt.Color;
+
 public enum Cor {
-    VERMELHO, VERDE, AZUL, AMARELO, ROXO, PRETO, BRANCO;
+    VERMELHO(new Color(255, 0,0)),
+    VERDE(new Color(0, 255,0)),
+    AZUL(new Color(0, 0,255)),
+    AMARELO(new Color(242, 255,0)),
+    ROXO(new Color(110, 0,150)),
+    PRETO(new Color(0,0,0)),
+    BRANCO(new Color(255,255,255)),
+    CINZA(new Color(60,65,70));
+
+    public final Color color;
+
+    Cor(Color color) {
+        this.color = color;
+    }
 }
